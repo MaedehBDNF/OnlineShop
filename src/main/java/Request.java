f2f12c60cc72fb36e2d2d864e5adacf11df5aadc
MaedehBDNF <badanfirouzm@gmail.com>
2023-03-30 17:27:56 +0330

    add Request class

---
+++ src/main/java/Request.java
@@ -0,0 +1,24 @@
+import java.util.UUID;
+
+public abstract class Request {
+    private UUID id;
+    private boolean isSubmitted;
+
+    public Request() {
+        this.id = UUID.randomUUID();
+        this.isSubmitted = false;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public boolean isSubmitted() {
+        return isSubmitted;
+    }
+
+    public void setSubmitted(boolean submitted) {
+        isSubmitted = submitted;
+    }
+
+}
