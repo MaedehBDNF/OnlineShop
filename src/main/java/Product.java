74a5b001e28ad2e2d678b6efc62ad984098f4ee2
MaedehBDNF <badanfirouzm@gmail.com>
2023-03-30 17:29:51 +0330

    add Product class

---
+++ src/main/java/Product.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public abstract class Product {
+    private String name;
+    private double price;
+    private Seller seller;
+    private int quantity;
+    private enum category {ELECTRONICS, CLOTHES, BOOKS, TOOLS, FURNITURE, JEWELRY, KITCHEN_UTENSILS, VEHICLE, STATIONERY, TOYS}
+    private ArrayList<String> comments = new ArrayList<String>();
+
+    public Product(String name, double price, Seller seller, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.seller = seller;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public ArrayList<String> getComments() {
+        return comments;
+    }
+
+    public void setComments(ArrayList<String> comments) {
+        this.comments = comments;
+    }
+}
