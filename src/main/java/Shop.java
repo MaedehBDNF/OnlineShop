9eaae7dfd9457d39d57cf02f3eb8d45e8e53c018
MaedehBDNF <badanfirouzm@gmail.com>
2023-03-31 15:01:57 +0330

    -add methods for add new products -add a method for increase number of a product

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -89,4 +89,66 @@ public class Shop {
     public void setAdminsOfShop(ArrayList<Admin> adminsOfShop) {
         this.adminsOfShop = adminsOfShop;
     }
+
+    public void addNewTV(String name, double price, Seller seller, int quantity, String category, double inches, int pixels){
+        TV newProduct = new TV(name, price, seller, quantity, category, inches, pixels);
+        products.add(newProduct);
+    }
+
+    public void addNewCoat(String name, double price, Seller seller, int quantity, String category, float size, int pockets){
+        Coat newProduct = new Coat(name, price, seller, quantity, category, size, pockets);
+        products.add(newProduct);
+    }
+
+    public void addNewBook(String name, double price, Seller seller, int quantity, String category, String author, int yearOfPublish, String isbn){
+        Book newProduct = new Book(name, price, seller, quantity, category, author, yearOfPublish, isbn);
+        products.add(newProduct);
+    }
+
+    public void addNewHummer(String name, double price, Seller seller, int quantity, String category, String commodity, double mass){
+        Hummer newProduct = new Hummer(name, price, seller, quantity, category, commodity, mass);
+        products.add(newProduct);
+    }
+
+    public void addNewSeat(String name, double price, Seller seller, int quantity, String category, String commodityOfStructure, String commodityOfSeat){
+        Seat newProduct = new Seat(name, price, seller, quantity, category, commodityOfStructure, commodityOfSeat);
+        products.add(newProduct);
+    }
+
+    public void addNewRing(String name, double price, Seller seller, int quantity, String category, double size, String commodity){
+        Ring newProduct = new Ring(name, price, seller, quantity, category, size, commodity);
+        products.add(newProduct);
+    }
+
+    public void addNewPot(String name, double price, Seller seller, int quantity, String category, String commodity, double volume){
+        Pot newProduct = new Pot(name, price, seller, quantity, category, commodity, volume);
+        products.add(newProduct);
+    }
+
+    public void addNewCar(String name, double price, Seller seller, int quantity, String category, String manufacture, String gear, String color, int numOfSeats, int numOfDoors){
+        Car newProduct = new Car(name, price, seller, quantity, category, manufacture, gear, color, numOfSeats, numOfDoors);
+        products.add(newProduct);
+    }
+
+    public void addNewPen(String name, double price, Seller seller, int quantity, String category, String manufacture, float thickness, String color){
+        Pen newProduct = new Pen(name, price, seller, quantity, category, manufacture, thickness, color);
+        products.add(newProduct);
+    }
+
+    public void addNewPuzzle(String name, double price, Seller seller, int quantity, String category, int numOfPieces){
+        Puzzle newProduct = new Puzzle(name, price, seller, quantity, category, numOfPieces);
+        products.add(newProduct);
+    }
+
+    public boolean increaseNumberOfProduct(int numberOfIncrease, Product product){
+        if(this.products.contains(product)){
+            int indexOfProduct = products.indexOf(product);
+            product.setQuantity(product.getQuantity() + numberOfIncrease);
+            products.set(indexOfProduct, product);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
